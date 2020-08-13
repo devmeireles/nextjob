8e607157a3c0daf340234be5156d2c1c5775b4f3
Gabriel Meireles <gmeireles@2mundos.net>
2020-08-13 09:32:30 -0300

    Adjusting skill tests

--- controller/skill_test.go
+++ controller/skill_test.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"os"
 	"testing"
 
+	"github.com/devmeireles/jobfinder/models"
 	"github.com/devmeireles/jobfinder/utils"
 	"github.com/gorilla/mux"
 	"github.com/steinfletcher/apitest"
@@ -32,96 +34,107 @@ func TestSkills(t *testing.T) {
 	ts := httptest.NewServer(r)
 	defer ts.Close()
 
-	// t.Run("get all skills", func(t *testing.T) {
-	// 	apitest.New().
-	// 		Handler(r).
-	// 		Get("/skills").
-	// 		Expect(t).
-	// 		Status(http.StatusOK).
-	// 		End()
-	// })
+	t.Run("get all skills", func(t *testing.T) {
+		apitest.New().
+			Handler(r).
+			Get("/skills").
+			Expect(t).
+			Status(http.StatusOK).
+			End()
+	})
 
-	// t.Run("found", func(t *testing.T) {
-	// 	apitest.New().
-	// 		Handler(r).
-	// 		Get("/skill/1").
-	// 		Expect(t).
-	// 		Status(http.StatusOK).
-	// 		End()
-	// })
+	t.Run("found", func(t *testing.T) {
+		apitest.New().
+			Handler(r).
+			Get("/skill/40").
+			Expect(t).
+			Status(http.StatusOK).
+			End()
+	})
 
-	// t.Run("not found", func(t *testing.T) {
-	// 	apitest.New().
-	// 		Handler(r).
-	// 		Get("/skill/12390482390").
-	// 		Expect(t).
-	// 		Status(http.StatusNotFound).
-	// 		End()
-	// })
+	t.Run("not found", func(t *testing.T) {
+		apitest.New().
+			Handler(r).
+			Get("/skill/12390482390").
+			Expect(t).
+			Status(http.StatusNotFound).
+			End()
+	})
 
-	// t.Run("Try to get a wrong parsed url skill", func(t *testing.T) {
-	// 	apitest.New().
-	// 		Handler(r).
-	// 		Get("/skill/1x").
-	// 		Expect(t).
-	// 		Status(http.StatusInternalServerError).
-	// 		End()
-	// })
+	t.Run("Try to get a wrong parsed url skill", func(t *testing.T) {
+		apitest.New().
+			Handler(r).
+			Get("/skill/1x").
+			Expect(t).
+			Status(http.StatusInternalServerError).
+			End()
+	})
 
-	// t.Run("Create a skill", func(t *testing.T) {
-	// 	var skill = models.Skill{
-	// 		Status: 1,
-	// 		Title:  "Google Cloud+10xx",
-	// 	}
-	// 	skillSave, _ := json.Marshal(skill)
+	t.Run("Create a skill", func(t *testing.T) {
+		var skill = models.Skill{
+			Status: 1,
+			Title:  "Google Cloud+10xxx",
+		}
+		skillSave, _ := json.Marshal(skill)
 
-	// 	apitest.New().
-	// 		Handler(r).
-	// 		Post("/skill").
-	// 		JSON(skillSave).
-	// 		Expect(t).
-	// 		Status(http.StatusOK).
-	// 		End()
-	// })
+		apitest.New().
+			Handler(r).
+			Post("/skill").
+			JSON(skillSave).
+			Expect(t).
+			Status(http.StatusOK).
+			End()
+	})
 
-	// t.Run("Update a skill", func(t *testing.T) {
-	// 	var skill = models.Skill{
-	// 		Title: "Google Cloud+20xx",
-	// 	}
-	// 	skillSave, _ := json.Marshal(skill)
+	t.Run("Update a skill", func(t *testing.T) {
+		var skill = models.Skill{
+			Title: "Google Cloud+20xxx",
+		}
+		skillSave, _ := json.Marshal(skill)
 
-	// 	apitest.New().
-	// 		Handler(r).
-	// 		Post("/skill").
-	// 		JSON(skillSave).
-	// 		Expect(t).
-	// 		Status(http.StatusOK).
-	// 		End()
-	// })
+		apitest.New().
+			Handler(r).
+			Method(http.MethodPut).
+			URL("/skill/41").
+			JSON(skillSave).
+			Expect(t).
+			Status(http.StatusOK).
+			End()
+	})
 
-	// t.Run("Update a skill but with a existent title", func(t *testing.T) {
-	// 	var skill = models.Skill{
-	// 		Title: "SQLxx",
-	// 	}
-	// 	skillUpdate, _ := json.Marshal(skill)
+	t.Run("Update a skill but with a existent title", func(t *testing.T) {
+		var skill = models.Skill{
+			Title: "Google Cloud+20xx",
+		}
+		skillUpdate, _ := json.Marshal(skill)
 
-	// 	apitest.New().
-	// 		Handler(r).
-	// 		Method(http.MethodPut).
-	// 		URL("/skill/1").
-	// 		JSON(skillUpdate).
-	// 		Expect(t).
-	// 		Status(http.StatusInternalServerError).
-	// 		End()
-	// })
+		apitest.New().
+			Handler(r).
+			Method(http.MethodPut).
+			URL("/skill/41").
+			JSON(skillUpdate).
+			Expect(t).
+			Status(http.StatusInternalServerError).
+			End()
+	})
 
 	t.Run("delete a skill", func(t *testing.T) {
 		apitest.New().
 			Handler(r).
 			Method(http.MethodDelete).
-			URL("/skill/3").
+			URL("/skill/45").
 			Expect(t).
 			Status(http.StatusOK).
+			End()
+	})
+
+	t.Run("try to delete a unexistent skill", func(t *testing.T) {
+		apitest.New().
+			Handler(r).
+			Method(http.MethodDelete).
+			URL("/skill/4543543").
+			Expect(t).
+			Status(http.StatusNotFound).
 			End()
 	})
 }
