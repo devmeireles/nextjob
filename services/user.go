b78d9d19af25b3ed22fce83097266c4e7f69d62d
Gabriel Meireles <gmeireles@2mundos.net>
2020-08-15 18:43:05 -0300

    Adding post and put methods

--- services/user.go
+++ services/user.go
@@ -72,3 +72,23 @@ func DeleteUser(id int) (*models.User, error) {
 
 	return &user, nil
 }
+
+func CreateUserAddress(address *models.Address) (*models.Address, error) {
+	var db = utils.DBConn()
+
+	if err := db.Model(&models.Address{}).Create(&address).Error; err != nil {
+		return &models.Address{}, err
+	}
+
+	return address, nil
+}
+
+func UpdateUserAddress(address *models.Address) (*models.Address, error) {
+	var db = utils.DBConn()
+
+	if err := db.Model(&models.Address{}).Where("id = ?", address.UserID).Update(&address).Error; err != nil {
+		return &models.Address{}, err
+	}
+
+	return address, nil
+}
